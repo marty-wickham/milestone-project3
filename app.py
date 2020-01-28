97c7d1b6ca5435d9bf7677f3d550596b9f6f04b4
martycistudent <marty.wickham@gmail.com>
2020-01-28 17:18:15 +0000

    Major increase to project functionality. Add recipe page, edit recipe page, navbar, view recipe page, and styling

--- app.py
+++ app.py
@@ -17,11 +17,45 @@ def get_recipes():
     return render_template("index.html", recipes=mongo.db.recipes.find())
 
 
+@app.route('/view_recipe/<recipe_id>')
+def view_recipe(recipe_id):
+    the_recipe = mongo.db.recipes.find_one({'_id': ObjectId(recipe_id)})
+
+    return render_template('recipe.html', recipe=the_recipe)
+
 @app.route('/add_recipe')
 def add_recipe():
     return render_template("add-recipe.html")
 
+
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipes = mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
+
+    return redirect(url_for('get_recipes'))
+
+
+@app.route('/edit_recipe/<recipe_id>')
+def edit_recipe(recipe_id):
+    the_recipe = mongo.db.recipes.find_one({'_id': ObjectId(recipe_id)})
+
+    return render_template("edit-recipe.html", recipe=the_recipe)
+
+
+@app.route('/update_recipe/<recipe_id>', methods=['POST'])
+def update_recipe(recipe_id):
+    recipes = mongo.db.recipes
+    recipes.update(
+        {'_id': ObjectId(recipe_id)},
+        {'title': request.form.get('recipe_name').capitalize(),
+         'image_url': request.form.get('image_url'),
+         'description': request.form.get('recipe_description').capitalize(),
+         'ingredients': request.form.get('recipe_ingredients').capitalize(),
+         'method': request.form.get('recipe_method').capitalize()})
     
+    return redirect(url_for('get_recipes'))
+
 if (__name__) == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
